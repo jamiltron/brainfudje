776c04a57cfac5495c3e06e3765c11ec29c99336
jamiltron <justinanthonyhamilton@gmail.com>
2011-07-05 22:32:20 -0500

    improving functionality

--- src/brainfudje/core.clj
+++ src/brainfudje/core.clj
@@ -5,7 +5,7 @@
   (cond
    (>= curr (count input)) (count input) 
    (< curr 0) 0                          
-   (= (nth input curr) cha) (+ curr 1)   ; if found, return the index immediately after cha
+   (= (nth input curr) cha) (inc curr)   ; if found, return the index immediately after cha
    :else (recur input (func curr) cha func)))
 
 (defn bf-interp [input instr d-array ptr]
@@ -16,33 +16,35 @@
     (cond
      (= -1 top) (list d-array ptr) ; if the instruction pointer is out-of-bounds return
      (= top \>) (cond
-                 (= ptr (- (count d-array) 1)) (recur input (+ instr 1) d-array ptr)
-                 :else (recur input (+ instr 1) d-array (+ ptr 1)))
+                 (= ptr (dec (count d-array))) (recur input (inc instr) d-array ptr)
+                 :else (recur input (inc instr) d-array (inc ptr)))
      (= top \<) (cond
-                 (= ptr 0) (recur input (+ instr 1) d-array ptr)
-                 :else (recur input (+ instr 1) d-array (- ptr 1)))
-       (= top \+) (recur input (+ instr 1) (assoc d-array ptr (mod (+ cell 1) 256)) ptr)
-       (= top \-) (recur input (+ instr 1) (assoc d-array ptr (mod (- cell 1) 256)) ptr)
-       (= top \,) (let [in (int (first (read-line)))]
-                    (flush)
-                    (recur input (+ instr 1) (assoc d-array ptr (mod (+ cell in) 256)) ptr))
-       (= top \[) (if (= 0 (nth d-array ptr)) (recur input (find-char input instr \] inc) d-array ptr)
-                      (recur input (+ instr 1) d-array ptr))
-       (= top \]) (if  (not= 0 (nth d-array ptr)) (recur input (find-char input instr \[ dec) d-array ptr)
-                       (recur input (+ instr 1) d-array ptr))
-       (= top  \.) (do
-                     (println (char (nth d-array ptr)))
-                     (flush)
-                     (recur input (+ instr 1) d-array ptr))
-       :else (recur input (+ instr 1) d-array ptr))))
+                 (= ptr 0) (recur input (inc instr) d-array ptr)
+                 :else (recur input (inc instr) d-array (- ptr 1)))
+     (= top \+) (recur input (inc instr) (assoc d-array ptr (mod (inc cell) 256)) ptr)
+     (= top \-) (recur input (inc instr) (assoc d-array ptr (mod (dec cell) 256)) ptr)
+     (= top \,) (let [in (int (first (read-line)))]
+                  (flush)
+                  (recur input (inc instr) (assoc d-array ptr (mod (+ cell in) 256)) ptr))
+     (= top \[) (if (= 0 (nth d-array ptr)) (recur input (find-char input instr \] inc) d-array ptr)
+                    (recur input (inc instr) d-array ptr))
+     (= top \]) (if  (not= 0 (nth d-array ptr)) (recur input (find-char input instr \[ dec) d-array ptr)
+                     (recur input (inc instr) d-array ptr))
+     (= top  \.) (do
+                   (println (char (nth d-array ptr)))
+                   (flush)
+                   (recur input (inc instr) d-array ptr))
+     :else (recur input (inc instr) d-array ptr))))
 
-(defn bf-repl [d-array ptr]
+(defn bf-repl []
   "brainfudge repl, continually updates the d-array and pointer to current cell"
-  (do
-    (print "brainfudje> ")
-    (flush)
-    (let [input (read-line)
-          out-tup (bf-interp input 0 d-array ptr)]
-      (recur (first out-tup) (second out-tup)))))
+  (loop [d-array (vec (take 30000 (repeat 0)))
+         ptr 0]
+    (do
+      (print "brainfudje> ")
+      (flush)
+      (let [input (read-line)
+            out-tup (bf-interp input 0 d-array ptr)]
+        (recur (first out-tup) (second out-tup))))))
 
-(bf-repl (vec (take 30000 (repeat 0))) 0)
+(bf-repl)
